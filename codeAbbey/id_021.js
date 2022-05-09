c0bade7b402dcece4677f4e5fe7cffd558345c6f
loginvergessen <spam.anon007@protonmail.com>
2022-05-09 04:19:46 +0200

    added the array counters puzzle

--- codeAbbey/id_021.js
+++ codeAbbey/id_021.js
@@ -1,11 +1,12 @@
 /*
-This file changes all the time and is used to test stuff in a quick and easy way
+Title:  Array Counters
+Link:   https://www.codeabbey.com/index/task_view/array-counters
 */
 
 output(arrayCounters(input()));
 
 function arrayCounters(initValues) {
-    output = [];
+    const output = [];
     numbers = input()
                 .split(' ')
                 .map(str => Number(str));
@@ -13,12 +14,12 @@ function arrayCounters(initValues) {
     let map = new Map();
 
     for (const number of numbers) {
-        if (!map.has(number))
+        if (!map.has(number)) {
             map.set(number, 1);
+            continue;
+        }
         map.set(number, map.get(number) + 1);
     }
-
-    console.log(map);
 
     for (let i = 1; i <= map.size; i++) {
         output.push(map.get(i));
