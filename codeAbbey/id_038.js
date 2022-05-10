dc0ef52584665569e97bfd25a0013000a434a9c2
loginvergessen <spam.anon007@protonmail.com>
2022-05-11 01:20:45 +0200

    started a course with some basic htlm,css,js projects

---
+++ codeAbbey/id_038.js
@@ -0,0 +1,10 @@
+/*
+Title:  Quadratic Equation
+Link:   https://www.codeabbey.com/index/task_view/quadratic-equation
+*/
+
+output(quadraticEquation(input()));
+
+function quadraticEquation(testCases) {
+
+}
